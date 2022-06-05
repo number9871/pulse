a46d1168c2471abf6d2c5447d2eaba9ecd0f2f69
Artem Musin NB <war-data@yandex.ru>
2022-06-06 01:02:44 +0300

    Added catalog with tabs and reviews sections

--- src/js/script.js
+++ src/js/script.js
@@ -14,11 +14,14 @@
 //         ]
 //     });
 //   });
+
+
 const slider = tns({
 	container: '.carousel__inner',
 	slideBy: 'page',
 	controls: false,
-	nav: false
+	nav: false,
+	touch: true
 });
 
 document.querySelector('.prev').addEventListener('click', function () {
@@ -28,3 +31,26 @@ document.querySelector('.prev').addEventListener('click', function () {
   document.querySelector('.next').addEventListener('click', function () {
 	slider.goTo('next');
   });
+
+  (function($) {
+	$(function() {
+	  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+		$(this)
+		  .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+		  .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+	  });
+	});
+
+	function toggleSlide(item) {
+		$(item).each(function(i) {
+			$(this).on('click', function(e) {
+				e.preventDefault();
+				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+			})
+		})
+	};
+
+	toggleSlide ('.catalog-item__link');
+	toggleSlide ('.catalog-item__back')
+})(jQuery);
