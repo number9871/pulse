227b9a30628cbd587fdac14dc9b521873293db23
Artem Musin NB <war-data@yandex.ru>
2022-06-16 22:18:53 +0300

    Optimization

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,3 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//         speed: 1200,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron left solid.png"></img></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron right solid.png"></img></button>',
-//         responsive: [
-//             {
-//                 breakpoint: 768,
-//                 settings: {
-//                   dots: true,
-//                   arrows: false
-//                 }
-//             }
-//         ]
-//     });
-//   });
-
 new WOW().init();
 
 //Slider
